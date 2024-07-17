5eaa2352fa5c401299d550bd486c72824a907e06
triplej55555 <j.jeuschede@geodv.de>
2024-07-17 09:54:12 +0200

    Add files via upload

---
+++ docs/pwd/pwd.js
@@ -0,0 +1,10 @@
+        // Prompt the user for a password
+        var password = prompt("Enter the password:");
+
+        // Check if the entered password matches
+        if (password !== "GEODV5!") {
+            document.body.innerHTML = "Unauthorized Access!";
+        } else {
+            // If the password is correct, show the content
+            document.body.style.display = "block";
+        }
